fdfb8bdffc206b7bb438c179ca4cd3f2e2c2f269
khyjb1995 <yong950423@stu.xawl.edu.cn>
2017-11-13 15:04:27 +0800

    Variable Setup

--- app/src/main/java/com/example/khy_j/a410321171_exercise3/DetailsActivity.java
+++ app/src/main/java/com/example/khy_j/a410321171_exercise3/DetailsActivity.java
@@ -1,8 +1,68 @@
 package com.example.khy_j.a410321171_exercise3;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by khy_j on 2017/11/13.
  */
 
-public class DetailsActivity {
+public class DetailsActivity extends AppCompatActivity {
+    private TextView txtPlace;
+    private TextView txtDetail;
+    private ImageView imgPic;
+    private ImageButton btnMap;
+    private ImageButton btnWeb;
+    private ImageButton btnCall;
+    private Button btnReturn;
+
+    String strPlaces;
+    String strDetails;
+    String strPics;
+    Float strMapsLocX;
+    Float strMapsLocY;
+    String strWeb;
+    Integer strPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datails);
+
+        txtPlace = (TextView)findViewById(R.id.textView_place) ;
+        txtDetail = (TextView)findViewById(R.id.textView_detail) ;
+        imgPic = (ImageView)findViewById(R.id.imageView_pic);
+        btnMap = (ImageButton)findViewById(R.id.imageButton_map);
+        btnWeb = (ImageButton)findViewById(R.id.imageButton_web);
+        btnCall = (ImageButton)findViewById(R.id.imageButton_call);
+        btnReturn = (Button)findViewById(R.id.btn) ;
+
+        // 取得 bundle
+        Intent intent=this.getIntent();
+        Bundle bundle=intent.getExtras();
+        strPlaces = bundle.getString("PLACE");
+        strDetails = bundle.getString("DETAIL");
+        strPics = bundle.getString("PIC");
+        strMapsLocX = bundle.getFloat("MAPX");
+        strMapsLocY = bundle.getFloat("MAPY");
+        strWeb = bundle.getString("WEB");
+        strPhone = bundle.getInt("PHONE");
+
+        
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            finish();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
 }
--- app/src/main/java/com/example/khy_j/a410321171_exercise3/MainActivity.java
+++ app/src/main/java/com/example/khy_j/a410321171_exercise3/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.khy_j.a410321171_exercise3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,14 +13,36 @@ public class MainActivity extends AppCompatActivity {
     String[] strPlaces = new String[0];
     String[] strDetails = new String[0];
     String[] strPics = new String[0];
-    String[] strMapsLocX = new String[0];
-    String[] strMapsLocY = new String[0];
+    Float[] strMapsLocX = new Float[0];
+    Float[] strMapsLocY = new Float[0];
     String[] strWeb = new String[0];
-    String[] strPhone = new String[0];
+    Integer[] strPhone = new Integer[0];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstPlace = (ListView)findViewById(R.id.listView_place);
     }
+
+    private ListView.OnItemClickListener lstPreferListener = new ListView.OnItemClickListener()
+    {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Intent intent=new Intent();
+            intent.setClass(MainActivity.this,DetailsActivity.class);
+
+            Bundle bundle = new Bundle();
+            bundle.putString("PLACE", strPlaces[i]);
+            bundle.putString("DETAIL", strDetails[i]);
+            bundle.putString("PIC", strPics[i]);
+            bundle.putFloat("MAPX", strMapsLocX[i]);
+            bundle.putFloat("MAPY", strMapsLocY[i]);
+            bundle.putString("WEB", strWeb[i]);
+            bundle.putInt("PHONE", strPhone[i]);
+
+            startActivity(intent);
+        }
+    };
 }
